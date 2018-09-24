c200f39ad087d8780b0232abdd6019dba1b5f1ec
Blake <blake.sweet16@gmail.com>
2018-09-24 16:58:53 -0400

    Added Notification ability

--- ForgetMeNot/AppDelegate.swift
+++ ForgetMeNot/AppDelegate.swift
@@ -22,15 +22,36 @@
 
 import UIKit
 import UserNotifications
+import CoreLocation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
   var window: UIWindow?
+    let locationManager = CLLocationManager()
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    // Request permission to send notifications
+    let center = UNUserNotificationCenter.current()
+    center.requestAuthorization(options:[.alert, .sound]) { (granted, error) in }
     
+    locationManager.delegate = self
     return true
   }
-	
+}
+
+// MARK: - CLLocationManagerDelegate
+extension AppDelegate: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) { // Call when exit a region
+        guard region is CLBeaconRegion else { return }
+        
+        let content = UNMutableNotificationContent() // Display an alert and sounds
+        content.title = "Forget Me Not"
+        content.body = "Are you forgetting something?"
+        content.sound = .default()
+        
+        let request = UNNotificationRequest(identifier: "ForgetMeNot", content: content, trigger: nil)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
 }
